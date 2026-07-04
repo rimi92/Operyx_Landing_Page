ad9c615ab188881011cf10ec0285557ac0c58871
rimi92 <rimghaoui@icloud.com>
2026-07-04 13:15:32 +0200

    Restore hero background

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -25,7 +25,7 @@ export default function Hero({ lang }: HeroProps) {
     <section
       id="hero"
       dir={isRTL ? 'rtl' : 'ltr'}
-     className="pt-[100px] pb-[40px] bg-red-500 md:bg-bleue-500"
+     className="pt-[100px] pb-[40px] bg-gradient-to-b from-[#BFE0FF] via-[#EAF4FF] to-[#FFFFFF]"
     >
       <div className="max-w-[1650px] mx-auto px-5">
 
